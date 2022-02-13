f74a6b3633d40dbda3553977869a6dabd7c2f0db
Jvco <65981077+zJvco@users.noreply.github.com>
2022-02-13 17:07:30 -0300

    Adding fontawesome to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function Header() {
     return (
         <header>
             <a href="/">My Calendar</a>
             <nav>
-                <button></button>
+                <button><FontAwesomeIcon icon="fa-solid fa-plus" /></button>
             </nav>
         </header>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './assets/style/index.css';
 import App from './App';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlus);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
