26fe78d336ec5ff33cbb60f99c64d33eb1eed1ff
Jvco <65981077+zJvco@users.noreply.github.com>
2022-02-13 16:44:27 -0300

    Structuring project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header>
+            <a href="/">My Calendar</a>
+            <nav>
+                <button></button>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
